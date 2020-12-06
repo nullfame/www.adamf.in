c0d2f82ebdd7a201818f4dedba53a8efac6b8068
Adam Finlayson <amf@adamfinlayson.com>
2020-12-06 16:30:44 -0600

    style: lint

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,6 +5,6 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Adam Finlayson',
-  plugins: []
-}
+  siteName: "Adam Finlayson",
+  plugins: [],
+};
