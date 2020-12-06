53b5ae8062313e2d997392ac2ab50a8b64a83532
Adam Finlayson <amf@adamfinlayson.com>
2020-12-06 17:08:57 -0600

    chore: config: gridsome-plugin-tailwindcss

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,5 +6,16 @@
 
 module.exports = {
   siteName: "Adam Finlayson",
-  plugins: [],
+  plugins: [
+    {
+      use: "gridsome-plugin-tailwindcss",
+      // * These are the default options.
+      // options: {
+      //   tailwindConfig: "./tailwind.config.js",
+      //   presetEnvConfig: {},
+      //   shouldImport: false,
+      //   shouldTimeTravel: false,
+      // },
+    },
+  ],
 };
