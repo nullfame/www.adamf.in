c82009890db55c032970b086fa185fb92fc22554
Adam Finlayson <amf@adamfinlayson.com>
2020-12-06 20:41:11 -0600

    add: content template

---
+++ src/templates/Content.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <article class="prose">
+      <h1>{{ $page.content.title }}</h1>
+      <div v-html="$page.content.content"></div>
+    </article>
+  </Layout>
+</template>
+
+<page-query>
+query Content ($path: String!) {
+	content (path: $path) {
+    title,
+    content
+  }
+}
+</page-query>
