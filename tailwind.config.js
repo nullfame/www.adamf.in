8cc4c8834c58d750c710489e8d1686c8be7fdae8
Adam Finlayson <amf@adamfinlayson.com>
2020-12-06 20:26:39 -0600

    chore: config: @tailwindcss/typography

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const typography = require("@tailwindcss/typography");
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [typography],
+};
