72492f120aea856c98f6680b8e7b53627d203929
Adam Finlayson <amf@adamfinlayson.com>
2020-12-13 22:30:38 -0600

    work: css grid responsive layout

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,18 @@
 <template>
-  <div>
-    <section>
+  <div
+    class="max-w-screen-sm mx-auto lg:max-w-screen-xl lg:mx-16 xl:mx-32 2xl:mx-auto lg:grid lg:grid-rows-4 lg:grid-cols-2 lg:gap-y-8 lg:gap-x-16 lg:my-16"
+  >
+    <section class="col-start-1 row-start-2">Photo</section>
+    <section class="col-start-1 row-start-1">
       <header>
-        <strong>
-          <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-        </strong>
+        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
       </header>
     </section>
-    <section>
+    <section class="col-start-1 row-start-3">Summary</section>
+    <section class="col-start-2 row-span-3">
       <slot />
     </section>
+    <section class="col-start-1 col-span-2">Footer</section>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,30 @@
+const plugin = require("tailwindcss/plugin");
 const typography = require("@tailwindcss/typography");
 
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        2: "3fr 5fr",
+      },
+      gridTemplateRows: {
+        4: "auto auto 1fr auto",
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [typography],
+  plugins: [
+    typography,
+    plugin(function ({ addBase }) {
+      addBase({
+        body: {
+          background: "#fffaf0",
+        },
+      });
+    }),
+  ],
 };
